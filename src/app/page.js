637f4a3014751c4c85385eebe8722d7886329347
agusasaad <agusasaad1099@hotmail.com>
2025-01-31 22:36:27 -0300

    add AboutMe component and update max-width in page styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import MainPage from "@/components/MainPage/MainPage";
 import styles from "./page.module.css";
+import AboutMe from "@/components/AboutMe/AboutMe";
 
 export default function Home() {
   return (
     <main className={styles.page}>
       <MainPage />
+      <AboutMe />
     </main>
   );
 }
