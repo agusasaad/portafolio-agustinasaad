c0044bc0ea35f1f6c0c550e2eadc6c0c12a1ef48
agusasaad <agusasaad1099@hotmail.com>
2025-02-11 17:28:01 -0300

    refactor NavBar component to use translation hooks and improve language change functionality

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,29 +3,33 @@ import Image from "next/image";
 import styles from "./NavBar.module.css";
 import user_profile from "@/assets/images/image_profile.jpg";
 import { useEffect, useRef, useState } from "react";
-import { gsap } from "gsap";
 import Link from "next/link";
 import Hamburguer from "@/assets/icons/Hamburguer";
 import Translate from "@/assets/icons/Translate";
 import ArrowDown from "@/assets/icons/ArrowDown";
+import { navBarAnimation } from "@/utils/animationGsap/AnimationGsap";
+import SwitchToggle from "../SwitchToggle/SwitchToggle";
+import { useLanguageStore } from "@/app/store";
+import { useTranslation } from "@/Hooks/useTranslations";
+import ChangeLanguage from "../ChangeLanguage/ChangeLanguage";
 
 const NavBar = () => {
+  const navBarContainer = useRef(null);
   const circleRef = useRef(null);
   const [showMenu, setShowMenu] = useState(false);
   const [showMenuLanguague, setShowMenuLanguague] = useState(false);
 
+  const t = useTranslation();
+
   useEffect(() => {
-    gsap.to(circleRef.current, {
-      scale: 2.5,
-      opacity: 0,
-      ease: "power1",
-      repeat: -1,
-      duration: 1.3,
-    });
+    navBarAnimation({ navBarContainer, circleRef });
   }, []);
 
   return (
-    <header className={`${styles.container} ${showMenu ? styles.show : ""}`}>
+    <header
+      className={`${styles.container} ${showMenu ? styles.show : ""}`}
+      ref={navBarContainer}
+    >
       <nav className={styles.nav_bar}>
         <div className={styles.avar_user}>
           <Link href={"/"} className={styles.container_image}>
@@ -47,7 +51,7 @@ const NavBar = () => {
                 href={"https://www.linkedin.com/in/agustin-asaad/"}
                 target="_blank"
               >
-                Available for Work
+                {t.navbar.user}
               </Link>
             </div>
           </div>
@@ -55,16 +59,16 @@ const NavBar = () => {
 
         <ul className={styles.list}>
           <Link href={"/"}>
-            <li>Home</li>
-            <li>Home</li>
+            <li>{t.navbar.home}</li>
+            <li>{t.navbar.home}</li>
           </Link>
           <Link href={"/projects"}>
-            <li>Projects</li>
-            <li>Projects</li>
+            <li>{t.navbar.projects}</li>
+            <li>{t.navbar.projects}</li>
           </Link>
           <Link href={"/contactme"}>
-            <li>Contact Me</li>
-            <li>Contact Me</li>
+            <li>{t.navbar.contact}</li>
+            <li>{t.navbar.contact}</li>
           </Link>
           <li className={styles.li_button}>
             <button
@@ -74,14 +78,10 @@ const NavBar = () => {
               <Translate />
               <ArrowDown />
             </button>
-            <div
-              className={`${styles.modal} ${
-                showMenuLanguague ? styles.showMenuLanguague : ""
-              }`}
-            >
-              <button>Español</button>
-              <button>English</button>
-            </div>
+            <ChangeLanguage
+              showMenuLanguague={showMenuLanguague}
+              setShowMenuLanguague={setShowMenuLanguague}
+            />
           </li>
         </ul>
 
