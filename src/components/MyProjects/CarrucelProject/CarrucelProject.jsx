37bbb03001eb2c51ccf574f67c9ab994f2dd13ef
agusasaad <agusasaad1099@hotmail.com>
2025-02-11 17:29:41 -0300

    refactor CarrucelProject component to use getProjects function; update project card layout and animation settings

--- src/components/MyProjects/CarrucelProject/CarrucelProject.jsx
+++ src/components/MyProjects/CarrucelProject/CarrucelProject.jsx
@@ -1,40 +1,37 @@
-import { projects } from "@/utils/Info";
+import { forwardRef } from "react";
+import { getProjects } from "@/utils/info/Info"; // Importa la función getProjects
 import styles from "./CarrucelProject.module.css";
 import Image from "next/image";
+import Link from "next/link";
 
-const CarrucelProject = () => {
+const CarrucelProject = forwardRef((props, ref) => {
+  const projects = getProjects();
+  const copyProject = [...projects, ...projects];
+
   return (
     <div className={styles.slider}>
       <div className={styles.move}>
-        {projects.map((project, index) => (
-          <div key={index} className={styles.card}>
+        {copyProject.map((project, index) => (
+          <Link
+            href={`/detail/${project.id}`}
+            key={index}
+            className={styles.card}
+            ref={(el) => (ref.current[index] = el)}
+          >
             <div className={styles.content_image}>
               <Image
                 src={project.imageCard}
-                width={500}
+                width={450}
                 height={500}
                 style={{ objectFit: "cover", objectPosition: "center" }}
                 alt={project.name}
               />
             </div>
-          </div>
-        ))}
-        {projects.map((project, index) => (
-          <div key={index} className={styles.card}>
-            <div className={styles.content_image}>
-              <Image
-                src={project.imageCard}
-                width={500}
-                height={500}
-                style={{ objectFit: "cover", objectPosition: "center" }}
-                alt={project.name}
-              />
-            </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
   );
-};
+});
 
 export default CarrucelProject;
