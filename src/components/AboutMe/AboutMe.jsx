f1eb4395cf8e37d7dee0e13d5a888c80b07a34f4
agusasaad <agusasaad1099@hotmail.com>
2025-02-11 17:28:45 -0300

    add AboutMe component with animation and translation support; update styles for improved layout

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,25 +1,34 @@
+"use client";
+import { useEffect, useRef } from "react";
 import SpiningText from "../SpiningText/SpiningText";
 import styles from "./AboutMe.module.css";
+import { aboutMeAnimation } from "@/utils/animationGsap/AnimationGsap";
+import { useTranslation } from "@/Hooks/useTranslations";
 
 const AboutMe = () => {
+  const container = useRef(null);
+  const imageProfile = useRef(null);
+  const title = useRef(null);
+  const paragraph = useRef(null);
+  const button = useRef(null);
+
+  //Zustand
+  const t = useTranslation();
+
+  useEffect(() => {
+    aboutMeAnimation({ container, imageProfile, title, paragraph, button });
+  }, []);
+
   return (
-    <section className={styles.container}>
-      <SpiningText />
-      <h2>About Me</h2>
-      <div className={styles.paragraph_content}>
-        <p>Hi there, I&apos;m Agustin – a Front End developer.</p>
-        <p>
-          I'm passionate about creating web applications that are easy to use
-          and enhance the user experience. Every challenge is an opportunity to
-          learn and bring creative solutions.
-        </p>
-        <p>
-          I&apos;m always looking to improve my skills and add value to every
-          project. I&apos;m motivated to work in teams, learn, and contribute to
-          collective success.
-        </p>
+    <section className={styles.container} ref={container}>
+      <SpiningText ref={imageProfile} />
+      <h2 ref={title}>{t.aboutMe.title}</h2>
+      <div className={styles.paragraph_content} ref={paragraph}>
+        <p>{t.aboutMe.paragraph_1}</p>
+        <p>{t.aboutMe.paragraph_2}</p>
+        <p>{t.aboutMe.paragraph_3}</p>
       </div>
-      <div className={styles.button_container}>
+      <div className={styles.button_container} ref={button}>
         <button>Curriculum Vitae</button>
       </div>
     </section>
