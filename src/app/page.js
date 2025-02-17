babbaa786c2941c2e541c45e6163e93237289d92
agusasaad <agusasaad1099@hotmail.com>
2025-02-17 10:56:26 -0300

    feat: implement dynamic import for MyProjects component to improve loading performance

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
 "use client";
+import dynamic from "next/dynamic";
 import MainPage from "@/components/MainPage/MainPage";
 import styles from "./page.module.css";
 import AboutMe from "@/components/AboutMe/AboutMe";
-import MyProjects from "@/components/MyProjects/MyProjects";
 import MyServices from "@/components/MyServices/MyServices";
 import ContactMe from "@/components/ContactMe/ContactMe";
+
+const MyProjects = dynamic(() => import("@/components/MyProjects/MyProjects"), {
+  ssr: false,
+  loading: () => <p>Cargando proyectos...</p>,
+});
 
 export default function Home() {
   return (
