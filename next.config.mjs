2344dd885ef096a44d038ed498d1771421278e6a
agusasaad <agusasaad1099@hotmail.com>
2025-02-19 14:56:49 -0300

    fix: remove Next.js configuration settings for SWC minification, strict mode, image sizes, font optimization, and custom webpack settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  swcMinify: true, // Minimiza el código JS con SWC (más rápido que Terser)
-  reactStrictMode: true, // Activar el modo estricto de React para detectar errores de manera temprana
-  images: {
-    deviceSizes: [320, 420, 768, 1024, 1200], // Ajustar tamaños de imágenes para diferentes dispositivos
-  },
-  experimental: {
-    optimizeFonts: true, // Optimización de fuentes para cargar solo las necesarias
-  },
-  webpack(config, { isServer }) {
-    if (!isServer) {
-      config.optimization.splitChunks.cacheGroups = {
-        default: false,
-        vendors: false,
-      };
-    }
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
