cbf34cec9945618ccffeba5510e8df37e337c668
agusasaad <agusasaad1099@hotmail.com>
2025-02-19 17:35:06 -0300

    fix: update HTML lang attribute from English to Spanish

--- src/app/layout.js
+++ src/app/layout.js
@@ -41,7 +41,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body className={poppins.className}>
         <NavBar />
         <CursorPoiter />
