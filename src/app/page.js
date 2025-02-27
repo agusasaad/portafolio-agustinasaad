a7ec91f5b4b53634b3b93ce7e427353370135b78
agusasaad <agusasaad1099@hotmail.com>
2025-02-27 10:43:28 -0300

    fix: change MainPage import to static for improved readability

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
 "use client";
 import dynamic from "next/dynamic";
 import styles from "./page.module.css";
+import MainPage from "@/components/MainPage/MainPage";
 
-const MainPage = dynamic(() => import("@/components/MainPage/MainPage"));
 const AboutMe = dynamic(() => import("@/components/AboutMe/AboutMe"));
 const MyProjects = dynamic(() => import("@/components/MyProjects/MyProjects"));
 const MyServices = dynamic(() => import("@/components/MyServices/MyServices"));
