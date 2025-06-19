d659b49055f0622bef7fa45ee43a192d5dc585ce
agusasaad <agusasaad1099@hotmail.com>
2025-06-19 12:39:01 -0300

    fix: update image properties and styles in CarrucelMobile component for improved layout

--- src/components/MyProjects/CarrucelMobile/CarrucelMobile.jsx
+++ src/components/MyProjects/CarrucelMobile/CarrucelMobile.jsx
@@ -1,11 +1,11 @@
-import { getProjects } from "@/utils/info/Info";
-import styles from "./CarrucelMobile.module.css";
-import Link from "next/link";
-import Image from "next/image";
-import { forwardRef } from "react";
+import { getProjects } from '@/utils/info/Info'
+import styles from './CarrucelMobile.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
+import { forwardRef } from 'react'
 
 const CarrucelMobile = forwardRef((props, ref) => {
-  const projects = getProjects();
+  const projects = getProjects()
 
   return (
     <div className={styles.slider}>
@@ -20,16 +20,21 @@ const CarrucelMobile = forwardRef((props, ref) => {
             <div className={styles.content_image}>
               <Image
                 src={project.imageCard}
+                alt={project.name}
                 width={340}
                 height={450}
-                alt={project.name}
+                style={{
+                  objectFit: 'cover',
+                  borderRadius: '15px',
+                  objectPosition: 'center',
+                }}
               />
             </div>
           </Link>
         ))}
       </div>
     </div>
-  );
-});
+  )
+})
 
-export default CarrucelMobile;
+export default CarrucelMobile
